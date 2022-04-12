d659a3493409c3ea65932cdaca7ee39a78791206
franpd8 <102958579+franpd8@users.noreply.github.com>
2022-04-12 21:03:48 +0200

    web personal vers 1.0

---
+++ clase7-javascript2/main.js
@@ -0,0 +1,15 @@
+// PARTE 1: BOOLEANO
+
+// && And
+// || or
+// ! distinto 
+
+const booleano1 = true
+console.log("booleano1",booleano1)
+const booleano2 = false 
+console.log("booleano2",booleano2)
+
+const booleanoAnd = booleano1 && booleano2
+
+console.log("Comparación AND",booleano2)
+
