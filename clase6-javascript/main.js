9cca01c7e668ffd407f116d04ed7a87e6e00b35b
franpd8 <102958579+franpd8@users.noreply.github.com>
2022-04-12 13:10:58 +0200

    clase 6

---
+++ clase6-javascript/main.js
@@ -0,0 +1,21 @@
+// Buena practica para saber si hemos enlazado el script al html 
+// alert('ey')
+
+
+const s ="Hello, World";
+let val;
+val = s.length;
+// .length nos devuelve el número de caracteres
+
+val = s.toUpperCase();
+// nos cambia el texto a mayusculas
+
+console.log(val)
+
+val = s.substring(5,9);
+console.log(val)
+// nos devuelve el contenido desde el 5to al 9no caracter
+
+val =s.split("",3)
+console.log(val)
+// nos devuelve por separado los 3ros valores 
